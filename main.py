1a8f9db1f31470506d33f41978272f7d775226fb
Predrag777 <predragbrajovicSS@gmail.com>
2024-10-19 11:46:19 +0200

    SS

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.autograd as autograd
+from sympy.unify.core import Variable
+from torch.autograd import Variable
+from collections import deque, namedtuple
+from network import Brain
+
+
+import gymnasium as gym
+env=gym.make('LunarLander-V2')
