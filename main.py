f2133b410b7d7a85deac87974723b49e682c8d78
Predrag777 <predragbrajovicSS@gmail.com>
2024-10-19 11:59:16 +0200

    SS

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from torch.autograd import Variable
 from collections import deque, namedtuple
 from network import Brain
 
+learning_rate=5e-4
 
 import gymnasium as gym
 env=gym.make('LunarLander-v3')
@@ -18,3 +19,11 @@ state_shape=env.observation_space.shape
 state_size=env.observation_space.shape[0]
 number_action=env.action_space.n
 print(number_action)
+
+
+miibatch_size=100
+gama=0.99                   #discount factor
+replay_buffer_size=int(1e5) #memory of the agent
+interpolation_parameter=1e-3 #NE ZNAM STA JE OVO
+
+print("SS")
