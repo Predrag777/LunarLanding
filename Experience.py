f1e386768ba006f87f527ed369c473ca65087737
Predrag777 <predragbrajovicSS@gmail.com>
2024-10-19 12:50:44 +0200

    WEW

--- Experience.py
+++ Experience.py
@@ -21,3 +21,13 @@ class ReplayMemory(object):
         self.memory.append(event)
         if len(self.memory)>self.capacity:
             del self.memory[0]
+    
+        def sample(self, batch_size):
+            experiences=random.sample(self.memory, k=batch_size)
+            states=torch.from_numpy(np.vstack([e[0] for e in experiences if e is not None])).float().to(self.device)
+            actions=torch.from_numpy(np.vstack(e[1] for e in experiences if e is not None)).float().to(self.device)
+            rewards=torch.from_numpy(np.vstack([e[2] for e in experiences if e is not None])).float().to(self.device)
+            next_states=torch.from_numpy(np.vstack(e[3] for e in experiences if e is not None)).float().to(self.device)
+            dones=torch.from_numpy(np.vstack(e[3] for e in experiences if e is not None)).astype(np.uint8).float().to(self.device)
+
+            return states, next_states, actions, rewards, dones
