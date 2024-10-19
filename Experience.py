c20fe2b7cec1e55200fa6f8bf130ea6be538aaab
Predrag777 <predragbrajovicSS@gmail.com>
2024-10-19 12:35:10 +0200

    WEW

---
+++ Experience.py
@@ -0,0 +1,18 @@
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.autograd as autograd
+from sympy.unify.core import Variable
+from torch.autograd import Variable
+from collections import deque, namedtuple
+from network import Brain
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.device=torch.devicee("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.capacity=capacity
+        self.memory=[]
