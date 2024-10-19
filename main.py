7df20199a230354deeb99916236555b5d4fa7b4b
Predrag777 <predragbrajovicSS@gmail.com>
2024-10-19 11:53:04 +0200

    SS

--- main.py
+++ main.py
@@ -13,4 +13,8 @@ from network import Brain
 
 
 import gymnasium as gym
-env=gym.make('LunarLander-V2')
+env=gym.make('LunarLander-v3')
+state_shape=env.observation_space.shape
+state_size=env.observation_space.shape[0]
+number_action=env.action_space.n
+print(number_action)
